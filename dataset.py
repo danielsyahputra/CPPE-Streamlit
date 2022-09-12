300c92ce85662caea2366bb5eadfda973e450c09
danielsyahputra13 <dnlshp@gmail.com>
2022-09-12 12:26:29 +0700

    add code for downloading data and models

--- dataset.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import utils
-
-if __name__=="__main__":
-    # utils.download_data(dir_name="./data")
-    # train_loader, test_loader = utils.dataloader()
-    pass
---
+++ download.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+import utils
+
+if __name__=="__main__":
+    utils.download_data()
+    utils.download_model()
